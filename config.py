adee8f74d9da12e3834131bd04246e6dbee83f26
proKBD <96772694+proKBD@users.noreply.github.com>
2025-03-07 17:16:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 CONFIG = {
-    "TAVILY_API_KEY": "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",  # Replace with your real Tavily key
+    "TAVILY_API_KEY": "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",  
     "MISTRAL_MODEL_PATH": "D:\\deep_research_ai\\models\\mistral-7b-instruct-v0.2.Q4_0.gguf"
-  # Path where you kept Mistral
 }
